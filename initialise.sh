c0396d4cd1882d43b5ba0c810bdfc82d8e878208
Anne-Gwenn Kettunen <uniaika@crypto-keupone.eu>
2015-11-07 18:03:27 +0100

    bleh

--- initialise.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "[+] Purging potential databases"
-mix ecto.drop
-echo "[+] Creating Database…"
-mix ecto.create -r Queerlink.Repo
-echo "[+] Migrating Database…"
-mix ecto.migrate -r Queerlink.Repo
-
