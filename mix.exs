7defbe5efa03ae3f85b978b4684f5a411010acac
Hécatée Irssiainen <kleidukos@crypto-keupone.eu>
2017-12-20 00:36:30 +0100

    Releases!

--- mix.exs
+++ mix.exs
@@ -34,6 +34,7 @@ defmodule Queerlink.Mixfile do
   defp deps do
     [
       {:cowboy, "~> 1.0"},
+      {:distillery, "~> 1.5", runtime: false, only: :prod},
       {:ex_doc, "~> 0.18.1"},
       {:gettext, "~> 0.11"},
       {:hashids, "~> 2.0.4"},
