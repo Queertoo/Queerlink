c12ab955bca7952b76165d0dd4c034b3c11a776b
Hécatée Irssiainen <kleidukos@crypto-keupone.eu>
2018-01-10 02:16:51 +0100

    bump version to 2.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Queerlink.Mixfile do
   def project do
     [
       app: :queerlink,
-      version: "2.0.0",
+      version: "2.0.1",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
